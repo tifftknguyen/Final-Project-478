1c0bdb020b4aac5d31dc504c6ce2309312d4ebb9
gissyhernandez <gisellehernandez907@gmail.com>
2025-12-05 22:02:42 -0800

    Added Background Info to Format String file

---
+++ scanner/format_string_guardian.c
@@ -0,0 +1,7 @@
+// Giselle Hernandez 
+// Tiffany Nguyen
+// CECS 478 Final Project 
+ 
+// Format String Guardian :
+// Using AST-based static scanner to test for unsafe string formatting 
+// As stated in our final proposal
